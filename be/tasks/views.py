b02c2116d9e3943d29c71e49b8f32ebafd134151
abdullah222888 <abdullahsana691@gmail.com>
2025-05-04 16:28:15 +0500

    done with APIView

--- be/tasks/views.py
+++ be/tasks/views.py
@@ -1,10 +1,24 @@
 from .models import Task
 from .serializers import TaskSerializer
-from rest_framework.generics import ListCreateAPIView
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 
 class Taskslists(ListCreateAPIView):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+# class DetailsOfTasks(RetrieveUpdateDestroyAPIView):
+#     serializer_class = TaskSerializer
+#     queryset = Task.objects.all()
+
+
+# class Taskslists2(APIView):
+#     def get(self, request):
+#         tasks = Task.objects.all()
+#         serializer = TaskSerializer(tasks, many=True)
+#         return Response(serializer.data)
+
